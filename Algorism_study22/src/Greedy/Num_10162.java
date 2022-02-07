dc429d2a9a830b46ecec3bc49342044fc0bad6e1
jungdahae1225 <jungdahae1225@gmail.com>
2022-02-07 16:47:34 +0900

    Num_10162_easy

---
+++ Algorism_study22/src/Greedy/Num_10162.java
@@ -0,0 +1,45 @@
+package Greedy;
+
+import java.util.Scanner;
+
+//처음엔 중복조합으로 풀려고 했다,,
+/***
+ * 
+ * @author dahae
+ * 성공
+ * 답안 확인 완료
+ * 11047과 비슷한 문제
+ */
+public class Num_10162 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt(); //입력이 초로 들어옴
+		
+		int A = 0;
+		int B = 0;
+		int C = 0;
+		
+		if(T >= 300) {
+			A = T/300;
+			T -= 300 * A;
+		}
+		if(T >= 60) {
+			B = T/60;
+			T -= 60 * B;
+		}
+		
+		//예외처리
+		if(T % 10 != 0) {
+			System.out.println(-1); 
+			System.exit(0); //프로그램 정상 종료 시키기
+		}
+		else {
+			C = T/10;
+			System.out.println(A + " " + B + " " + C);
+		}
+		sc.close();
+	}
+
+}
